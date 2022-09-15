54617a1038323089214806917df227be5f1617d4
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-09-15 08:09:35 +0100

    initial commit

---
+++ download_grids.py
@@ -0,0 +1,23 @@
+
+
+import os
+
+
+grid_url = {}
+grid_url[] = ''
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    synthesizer_data_dir = os.getenv('SYNTHESIZER_DATA')
+
+    grids = []
+    grids += []
