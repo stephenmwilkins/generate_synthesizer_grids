0170aa50cb1fe429f171f36a8631a55c81c6bf39
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-09-15 14:11:16 +0100

    updated

--- cloudy/convert_cloudy_grid_to_hdf5.py
+++ cloudy/convert_cloudy_grid_to_hdf5.py
@@ -28,76 +28,94 @@ synthesizer_data_dir = os.getenv('SYNTHESIZER_DATA')
 path_to_grids = f'{synthesizer_data_dir}/grids'
 path_to_cloudy_files = f'{synthesizer_data_dir}/cloudy'
 
-sps_model = 'bpass-v2.2.1-bin_chab-100' # --- the SPS/IMF model
-cloudy_model = 'cloudy-v17.0_logUref-2' # --- the cloudy grid
-# spec_names = ['incident','transmitted','nebular','nebular_continuum','total','linecont']
-spec_names = ['incident','transmitted','nebular','linecont'] # --- the cloudy spectra to save (others can be generated later)
 
+cloudy_models = ['cloudy-v17.03_logUref-2'] # --- the cloudy grid
 
-# --- open the original SPS model grid
-fn = f'{path_to_grids}/{sps_model}.h5'
-hf_sps = h5py.File(fn,'r')
-
-# --- open the new grid file
-fn = f'{path_to_grids}/{sps_model}_{cloudy_model}.h5' # the new cloudy grid
-hf = h5py.File(fn,'w')
-
-# --- copy various quantities (all excluding the spectra) from the original sps grid
-for ds in ['metallicities', 'log10metallicities', 'log10ages', 'log10Q', 'star_fraction', 'remnant_fraction']:
-    hf_sps.copy(hf_sps[ds], hf['/'], ds)
-
-# --- short hand for later
-metallicities = hf['metallicities']
-log10ages = hf['log10ages']
-
-nZ = len(metallicities)  # number of metallicity grid points
-na = len(log10ages)  # number of age grid points
-
-
-# --- read first spectra from the first grid point to get length and wavelength grid
-lam = read_wavelength(f'{path_to_cloudy_files}/{sps_model}_{cloudy_model}/0_0')
-
-
-spectra = hf.create_group('spectra')  # create a group holding the spectra in the grid file
-spectra.attrs['spec_names'] = spec_names  # save list of spectra as attribute
-
-spectra['wavelength'] = lam  # save the wavelength
-
-nlam = len(lam)  # number of wavelength points
-
-# --- make spectral grids and set them to zero
-for spec_name in spec_names:
-    spectra[spec_name] = np.zeros((na, nZ, nlam))
-
-
-# --- now loop over meallicity and ages
-
-for iZ, Z in enumerate(metallicities):
-    for ia, log10age in enumerate(log10ages):
-
-
-        infile = f'{path_to_cloudy_files}/{sps_model}_{cloudy_model}/{ia}_{iZ}'
-
-        if os.path.isfile(infile+'.cont'):  # attempt to open run.
-            exists = 'exists'
-            spec_dict = read_continuum(infile, return_dict = True)
-            for spec_name in spec_names:
-                spectra[spec_name][ia, iZ] = spec_dict[spec_name]
-
-            # --- we need to rescale the cloudy spectra to the original SPS spectra. This is done here using the ionising photon luminosity, though could in principle by done at a fixed wavelength.
-
-            log10Q = np.log10(calculate_Q(lam, spectra['incident'][ia, iZ, :]))  # calculate log10Q
-
-            for spec_name in spec_names:
-                spectra[spec_name][ia, iZ] *= 10**(hf['log10Q'][ia, iZ] - log10Q)
-
-        else:
-            exists = 'does not exist'
+sps_grids = [
+    'bc03_chabrier03',
+    'bpass-v2.2.1-bin_100-100',
+    'bpass-v2.2.1-bin_100-300',
+    'bpass-v2.2.1-bin_135-100',
+    'bpass-v2.2.1-bin_135-300',
+    'bpass-v2.2.1-bin_135all-100',
+    'bpass-v2.2.1-bin_170-100',
+    'bpass-v2.2.1-bin_170-300',
+    # 'bpass-v2.2.1-bin_chab-100',
+    'bpass-v2.2.1-bin_chab-300',
+    'maraston-rhb_kroupa',
+    'maraston-rhb_salpeter',
+]
 
 
 
+for sps_grid in sps_grids:
+
+    for cloudy_model in cloudy_models:
 
 
+        # spec_names = ['incident','transmitted','nebular','nebular_continuum','total','linecont']
+        spec_names = ['incident','transmitted','nebular','linecont'] # --- the cloudy spectra to save (others can be generated later)
 
-hf.visit(print)
-hf.flush()
+
+        # --- open the original SPS model grid
+        fn = f'{path_to_grids}/{sps_model}.h5'
+        hf_sps = h5py.File(fn,'r')
+
+        # --- open the new grid file
+        fn = f'{path_to_grids}/{sps_model}_{cloudy_model}.h5' # the new cloudy grid
+        hf = h5py.File(fn,'w')
+
+        # --- copy various quantities (all excluding the spectra) from the original sps grid
+        for ds in ['metallicities', 'log10metallicities', 'log10ages', 'log10Q', 'star_fraction', 'remnant_fraction']:
+            hf_sps.copy(hf_sps[ds], hf['/'], ds)
+
+        # --- short hand for later
+        metallicities = hf['metallicities']
+        log10ages = hf['log10ages']
+
+        nZ = len(metallicities)  # number of metallicity grid points
+        na = len(log10ages)  # number of age grid points
+
+
+        # --- read first spectra from the first grid point to get length and wavelength grid
+        lam = read_wavelength(f'{path_to_cloudy_files}/{sps_model}_{cloudy_model}/0_0')
+
+
+        spectra = hf.create_group('spectra')  # create a group holding the spectra in the grid file
+        spectra.attrs['spec_names'] = spec_names  # save list of spectra as attribute
+
+        spectra['wavelength'] = lam  # save the wavelength
+
+        nlam = len(lam)  # number of wavelength points
+
+        # --- make spectral grids and set them to zero
+        for spec_name in spec_names:
+            spectra[spec_name] = np.zeros((na, nZ, nlam))
+
+
+        # --- now loop over meallicity and ages
+
+        for iZ, Z in enumerate(metallicities):
+            for ia, log10age in enumerate(log10ages):
+
+
+                infile = f'{path_to_cloudy_files}/{sps_model}_{cloudy_model}/{ia}_{iZ}'
+
+                if os.path.isfile(infile+'.cont'):  # attempt to open run.
+                    exists = 'exists'
+                    spec_dict = read_continuum(infile, return_dict = True)
+                    for spec_name in spec_names:
+                        spectra[spec_name][ia, iZ] = spec_dict[spec_name]
+
+                    # --- we need to rescale the cloudy spectra to the original SPS spectra. This is done here using the ionising photon luminosity, though could in principle by done at a fixed wavelength.
+
+                    log10Q = np.log10(calculate_Q(lam, spectra['incident'][ia, iZ, :]))  # calculate log10Q
+
+                    for spec_name in spec_names:
+                        spectra[spec_name][ia, iZ] *= 10**(hf['log10Q'][ia, iZ] - log10Q)
+
+                else:
+                    exists = 'does not exist'
+
+        hf.visit(print)
+        hf.flush()
--- sps/install_fsps.py
+++ sps/install_fsps.py
@@ -1,72 +1,95 @@
 import sys
-
 import numpy as np
 import fsps
-
+from synthesizer.sed import convert_flam_to_fnu, calculate_Q
 from utils import write_data_h5py, write_attribute
 
 
-def grid(Nage=80, NZ=20, zsolar=0.0142):
-    """
-    Generate grid of spectra with FSPS
-
-    Returns:
-        spec (array, float) spectra, dimensions NZ*Nage
-        metallicities (array, float) metallicity array, units log10(Z)
-        ages (array, flota) age array, units years
-        wl (array, float) wavelength array, units Angstroms
-    """
-
-    sp = fsps.StellarPopulation(zcontinuous=1, sfh=0, imf_type=2, sf_start=0.)
-
-    wl = sp.wavelengths  # units: Angstroms
-    ages = sp.log_age  # units: log10(years)
-    metallicities = np.log10(sp.zlegend)  # units: log10(Z)
-
-    spec = np.zeros((len(metallicities), len(ages), len(wl)))
-    Z_solar = 0.0127
-
-    for i, Z in enumerate(metallicities):
-        print(i, Z)
-        for j, a in enumerate(10**ages / 1e9):
-            sp.params['logzsol'] = Z - np.log10(Z_solar)
-            spec[i, j] = sp.get_spectrum(tage=a, peraa=True)[1]   # Lsol / AA
-
-    # convert spec units
-    spec *= (3.826e33 / 1.1964952e40)  # erg / s / cm^2 / AA
-
-    return spec, ages, metallicities, wl
+imf_key = {
+    'Salpeter' : 0,
+    'Chabrier03' : 1,
+    'Kroupa01' : 2,
+}
 
 
-def main(outfile='output/fsps.h5'):
+def generate_grid(imf):
     """ Main function to create fsps grids used by synthesizer """
-    Nage = 81
-    NZ = 41
 
-    spec, age, Z, wl = grid(Nage=Nage, NZ=NZ)
 
-    write_data_h5py(outfile, 'spectra', data=spec, overwrite=True)
-    write_attribute(outfile, 'spectra', 'Description',
-                    'Three-dimensional spectra grid, [Z,Age,wavelength]')
-    write_attribute(outfile, 'spectra', 'Units', 'erg s^-1 cm^2 AA^-1')
+    model_name = f'fsps-v3.2_{imf}'
+    out_filename = f'{grid_dir}/{model_name}.h5'
 
-    write_data_h5py(outfile, 'ages', data=age, overwrite=True)
-    write_attribute(outfile, 'ages', 'Description',
-                    'Stellar population ages in log10 years')
-    write_attribute(outfile, 'ages', 'Units', 'log10(yr)')
+    sp = fsps.StellarPopulation(imf_type = imf_key[imf]) # sfh = 0: ssp, zcontinuous = 0: use metallicity grid
 
-    write_data_h5py(outfile, 'metallicities', data=Z, overwrite=True)
-    write_attribute(outfile, 'metallicities', 'Description',
-                    'raw abundances in log10')
-    write_attribute(outfile, 'metallicities', 'Units',
-                    'dimensionless [log10(Z)]')
+    lam = sp.wavelengths  # units: Angstroms
+    log10ages = sp.log_age  # units: log10(years)
+    ages = 10**log10ages
+    metallicities = sp.zlegend  # units: log10(Z)
+    log10metallicities = np.log10(metallicities)
 
-    write_data_h5py(outfile, 'wavelength', data=wl, overwrite=True)
-    write_attribute(outfile, 'wavelength', 'Description',
-                    'Wavelength of the spectra grid')
-    write_attribute(outfile, 'wavelength', 'Units', 'AA')
+    na = len(log10ages)
+    nZ = len(metallicities)
+
+    log10Q = np.zeros((na,nZ)) # the ionising photon production rate
+    spec = np.zeros((na, nZ, len(lam)))
+
+    for iZ in range(nZ):
+        spec_ = sp.get_spectrum(zmet=iZ+1)[1]   # 2D array Lsol / AA
+        for ia in range(na):
+
+            flam = spec_[ia]  # Lsol / AA
+            flam *= 3.826e33  # erg s^-1 AA^-1 Msol^-1
+
+            fnu = convert_flam_to_fnu(lam, flam)
+            spec[ia, iZ] = fnu
+
+            log10Q[ia, iZ] = np.log10(calculate_Q(lam, fnu))
+
+
+
+
+    write_data_h5py(fname, 'spectra/wavelength', data=lam, overwrite=True)
+    write_attribute(fname, 'spectra/wavelength', 'Description',
+            'Wavelength of the spectra grid')
+    write_attribute(fname, 'spectra/wavelength', 'Units', 'AA')
+
+    write_data_h5py(fname, 'ages', data=ages, overwrite=True)
+    write_attribute(fname, 'ages', 'Description',
+            'Stellar population ages years')
+    write_attribute(fname, 'ages', 'Units', 'yr')
+
+    write_data_h5py(fname, 'log10ages', data=log10ages, overwrite=True)
+    write_attribute(fname, 'log10ages', 'Description',
+            'Stellar population ages in log10 years')
+    write_attribute(fname, 'log10ages', 'Units', 'log10(yr)')
+
+    write_data_h5py(fname, 'metallicities', data=metallicities, overwrite=True)
+    write_attribute(fname, 'metallicities', 'Description',
+            'raw abundances')
+    write_attribute(fname, 'metallicities', 'Units', 'dimensionless [Z]')
+
+    write_data_h5py(fname, 'log10metallicities', data=log10metallicities, overwrite=True)
+    write_attribute(fname, 'log10metallicities', 'Description',
+            'raw abundances in log10')
+    write_attribute(fname, 'log10metallicities', 'Units', 'dimensionless [log10(Z)]')
+
+    write_data_h5py(fname, 'log10Q', data=log10Q, overwrite=True)
+    write_attribute(fname, 'log10Q', 'Description',
+                                    'Two-dimensional ionising photon production rate grid, [age,Z]')
+
+    write_data_h5py(fname, 'spectra/stellar', data=spec, overwrite=True)
+    write_attribute(fname, 'spectra/stellar', 'Description',
+                    'Three-dimensional spectra grid, [age, metallicity, wavelength]')
+    write_attribute(fname, 'spectra/stellar', 'Units', 'erg s^-1 Hz^-1')
 
 
 if __name__ == "__main__":
-    outfile = sys.argv[1]
-    main(outfile)
+
+    grid_dir = f'{synthesizer_data_dir}/grids'
+
+    imfs = ['Salpeter', 'Chabrier03', 'Kroupa01']
+    imfs = ['Chabrier03']
+
+    for imf in imfs:
+
+         generate_grid(imf)
