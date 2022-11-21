cdba429a51fc235efca355944e1c85b226fac14b
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-11-21 11:22:02 +0000

    updated

--- cloudy/check_missing_models.py
+++ cloudy/check_missing_models.py
@@ -25,28 +25,32 @@ path_to_grids = f'{synthesizer_data_dir}/grids'
 path_to_cloudy_files = f'{synthesizer_data_dir}/cloudy'
 
 
-cloudy_models = ['cloudy-v17.03_log10Uref-2']  # --- the cloudy grid
+# cloudy_models = ['cloudy-v17.03_log10Uref-2']  # --- the cloudy grid
+#
+# sps_grids = [
+#     'bc03_chabrier03',
+#     'bpass-v2.2.1-bin_100-100',
+#     'bpass-v2.2.1-bin_100-300',
+#     'bpass-v2.2.1-bin_135-100',
+#     'bpass-v2.2.1-bin_135-300',
+#     'bpass-v2.2.1-bin_135all-100',
+#     'bpass-v2.2.1-bin_170-100',
+#     'bpass-v2.2.1-bin_170-300',
+#     'fsps-v3.2_Chabrier03',
+#     'bpass-v2.2.1-bin_chab-100',
+#     'bpass-v2.2.1-bin_chab-300',
+#     'maraston-rhb_kroupa',
+#     'maraston-rhb_salpeter',
+#     'bc03-2016-Stelib_chabrier03',
+#     'bc03-2016-BaSeL_chabrier03',
+#     'bc03-2016-Miles_chabrier03',
+# ]
 
-sps_grids = [
-    'bc03_chabrier03',
-    'bpass-v2.2.1-bin_100-100',
-    'bpass-v2.2.1-bin_100-300',
-    'bpass-v2.2.1-bin_135-100',
-    'bpass-v2.2.1-bin_135-300',
-    'bpass-v2.2.1-bin_135all-100',
-    'bpass-v2.2.1-bin_170-100',
-    'bpass-v2.2.1-bin_170-300',
-    'fsps-v3.2_Chabrier03',
-    'bpass-v2.2.1-bin_chab-100',
-    'bpass-v2.2.1-bin_chab-300',
-    'maraston-rhb_kroupa',
-    'maraston-rhb_salpeter',
-    'bc03-2016-Stelib_chabrier03',
-    'bc03-2016-BaSeL_chabrier03',
-    'bc03-2016-Miles_chabrier03',
-]
+# sps_grids = [f'fsps-v3.2_imf3:{imf3:.1f}' for imf3 in np.arange(1.5, 3.1, 0.1)]
 
-sps_grids = [f'fsps-v3.2_imf3:{imf3:.1f}' for imf3 in np.arange(1.5, 3.1, 0.1)]
+
+sps_grids = ['bpass-v2.2.1-bin_chab-300']
+cloudy_models = [f'cloudy-v17.03_log10Uref-{log10U:.1f}' for log10U in np.arange(-4, 1.1, 0.5)]
 
 
 for sps_model in sps_grids:
